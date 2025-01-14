d35ed1341a1c49018e7e10b9225d6508b8c28f08
MarcoRH04 <marco_1322134127@uptecamac.edu.mx>
2025-01-14 13:54:59 -0600

    03-ProductSearch

---
+++ 03-ProductSearch/app/src/main/java/com/example/a03_productsearch/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.a03_productsearch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a03_productsearch.ui.theme._03ProductSearchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _03ProductSearchTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryDropdown() {
+    val categories = listOf("Electrónica", "Ropa", "Hogar")
+    val expanded = remember { mutableStateOf(false) }
+    val selectedCategory = remember { mutableStateOf("") }
+
+    Column {
+        OutlinedTextField(
+            value = selectedCategory.value,
+            onValueChange = {},
+            label = { Text("Elige una categoría") },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Desplegar menú",
+                    modifier = Modifier.clickable {
+                        expanded.value = !expanded.value
+                    }
+                )
+            },
+            readOnly = true
+        )
+
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false }
+        ) {
+            categories.forEach { category ->
+                DropdownMenuItem(
+                    onClick = {
+                        selectedCategory.value = category
+                        expanded.value = false
+                    },
+                    text = { Text(category) }
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Buscar productos",
+                modifier = Modifier.padding(top = 10.dp, bottom = 6.dp),
+                fontSize = 30.sp
+            )
+            Text(text = "Nombre del producto")
+            OutlinedTextField(value = "", onValueChange = {})
+
+            Text(text = "Categoría")
+            CategoryDropdown()
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Buscar")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _03ProductSearchTheme {
+        Greeting("Android")
+    }
+}
